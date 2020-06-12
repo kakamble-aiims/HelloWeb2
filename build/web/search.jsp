27e05188131a0c71efac984285d95e2001cf6acc
Kshitij Kamble <kakamble.aiims@gmail.com>
2020-06-12 13:13:49 +0530

    First commit

---
+++ build/web/search.jsp
@@ -0,0 +1,44 @@
+<%-- 
+    Document   : search
+    Created on : 9 Jun, 2020, 10:59:23 AM
+    Author     : root
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html xmlns:h="http://xmlns.jcp.org/jsf/html">
+    <h:head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="search.css">
+        <script type="text/javascript" src="javascript_srch.js"></script>
+        <title>Search record</title>
+    </h:head>
+    <h:body>
+        <br>
+            <p>
+                <form name="search_rec">
+                    <br>Enter your name to search your record:
+                    <input type="text" name="name" value="" onkeyup="sendNameInfo()"/>
+                </form>
+            </p>
+            <p>
+                <br>
+                <br>
+            <table id="search_table" border = "1" width = "100%">
+                <th>
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Age</td>
+                        <td>Occupation</td>
+                        <td>Address</td>
+                        <td>Hobbies</td>
+                    </tr>
+                </th>
+                <tbody>  
+                </tbody>
+            </table>
+            </p>
+        </br>
+    </h:body>
+</html>
---
+++ src/java/org/newpackage/hello/DetailsHandler.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.newpackage.hello;
+
+/**
+ *
+ * @author root
+ */
+public class DetailsHandler {
+    private int id;
+    private String name;
+    private int age;
+    private String occupation;
+    private String address;
+    private String hobbies;
+    
+    public DetailsHandler() {
+        id = 0;
+        name = null;
+        age = 0;
+        occupation = null;
+        address = null;
+        hobbies = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+}
